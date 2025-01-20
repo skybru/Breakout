9666381c0d619a04c0675f62c95b5bb2ddbbb4b5
skybru <giorgio.bruschi98@gmail.com>
2025-01-20 15:35:07 +0100

    third lesson - bounce off the walls

--- game_engine.js
+++ game_engine.js
@@ -18,11 +18,22 @@ let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
+const ballRadius = 10;
+
+function checkBorderCollision() {
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+}
 
 //separate functions
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2, false);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2, false);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -31,6 +42,7 @@ function drawBall() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    checkBorderCollision();
     x += dx;
     y += dy;
 }
