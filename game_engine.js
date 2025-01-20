b81d84c4969a66620c7799f242e40340480946aa
skybru <giorgio.bruschi98@gmail.com>
2025-01-20 21:18:50 +0100

    sixth lesson - brick field

--- game_engine.js
+++ game_engine.js
@@ -26,6 +26,21 @@ let isRightPressed = false;
 let isLeftPressed = false;
 let interval = 0;
 
+const brickRowCount = 3;
+const brickColumnCount = 5;
+const brickWidth = 75;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
+const bricks = [];
+for (let c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (let r = 0; r < brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0};
+    }
+}
+
 function checkBorderCollision() {
     if ( y + dy < ballRadius) {
         dy = -dy;
@@ -62,10 +77,27 @@ function drawPaddle() {
     ctx.closePath();
 }
 
+function drawBricks() {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+            const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY;
+            ctx.beginPath();
+            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillStyle = "orange";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawPaddle();
     drawBall();
+    drawBricks();
     checkBorderCollision();
     x += dx;
     y += dy;
